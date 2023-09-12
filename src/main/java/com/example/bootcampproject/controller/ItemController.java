9f644ff0711b467e71f72aa692b729dbf4970c03
Viswonathan06 <viswonathan0606@gmail.com>
2023-09-12 16:08:00 +0530

    Item Entity Controllers Initialized

---
+++ src/main/java/com/example/bootcampproject/controller/ItemController.java
@@ -0,0 +1,5 @@
+package com.example.bootcampproject.controller;
+
+public class ItemController {
+    
+}
---
+++ src/main/java/com/example/bootcampproject/service/ItemServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.bootcampproject.service;
+
+public class ItemServiceImpl {
+    
+}
