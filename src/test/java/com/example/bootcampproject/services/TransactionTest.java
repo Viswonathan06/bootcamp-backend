86677442321ec8500acb5af5f9f268711ee3b002
Viswonathan06 <viswonathan0606@gmail.com>
2023-09-26 10:13:12 +0530

    Transaction tests added

--- src/test/java/com/example/bootcampproject/services/TransactionTest.java
+++ src/test/java/com/example/bootcampproject/services/TransactionTest.java
@@ -1,11 +1,13 @@
 package com.example.bootcampproject.services;
 
+import com.example.bootcampproject.dto.LoanTransactionDTO;
 import com.example.bootcampproject.entity.Employee;
 import com.example.bootcampproject.entity.EmployeeCardDetails;
 import com.example.bootcampproject.entity.EmployeeIssue;
 import com.example.bootcampproject.entity.Item;
 import com.example.bootcampproject.entity.LoanCard;
 import com.example.bootcampproject.entity.LoanTransaction;
+import com.example.bootcampproject.exceptions.ResourceNotFoundException;
 import com.example.bootcampproject.repository.LoanTransactionRepository;
 import com.example.bootcampproject.service.LoanTransactionService;
 import com.example.bootcampproject.service.LoanTransactionServiceImpl;
@@ -28,7 +30,7 @@ import org.mockito.InjectMocks;
 import static org.mockito.BDDMockito.*;
 import org.mockito.Mock;
 import org.mockito.ArgumentMatchers.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import org.mockito.Mockito.*;
 
 
@@ -74,10 +76,45 @@ public class TransactionTest {
 
     @Test
     public void givenTransactionId_thenReturnTransactionObj(){
-        given(loanTransactionRepository.findById(Long.valueOf(1))).willReturn(Optional.of(loanTransaction));
-        LoanTransaction savedTransaction = loanTransactionServiceImpl.getLoanTransactionById(Long.valueOf(loanTransaction.getTransactionId())).get(0);
-        assertThat(savedTransaction).isnotNull();
+        given(loanTransactionRepository.findById(1L)).willReturn(Optional.of(loanTransaction));
+
+        // when
+        try{
+            LoanTransaction savedLoan;
+            when(loanTransactionServiceImpl.getLoanTransactionById(Long.valueOf(loanTransaction.getTransactionId())).getBody()).thenReturn(savedLoan);
+            assertThat(savedLoan).isNotNull();
+        }catch(ResourceNotFoundException e){
+            System.out.println(e);
+        }
+
     }
+    @Test
+    public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject(){
+        // given - precondition or setup
+        given(loanTransactionRepository.findById(Long.valueOf(loanTransaction.getTransactionId())))
+                .willReturn(Optional.empty());
+
+        given(loanTransactionRepository.save(loanTransaction)).willReturn(loanTransaction);
+
+        System.out.println(loanTransactionRepository);
+        System.out.println(loanTransactionServiceImpl);
+        LoanTransactionDTO loanTransactionDTO = new LoanTransactionDTO(loanTransaction.getStatus(), false, loanTransaction.getLoanCard().getLoanType(),loanTransaction.getTransactionId(), 
+        loanTransaction.getTimestamp(), loanTransaction.getAmount(), loanTransaction.getLoanCard().getLoanId(), 
+        loanTransaction.getEmployee().getEmployeeId(), null , loanTransaction.getLoanCard().getDuration());;
+        // when -  action or the behaviour that we are going test
+        try{
+            LoanTransaction savedTransaction = loanTransactionServiceImpl.registerLoanTransaction(loanTransactionDTO).getBody();
+            System.out.println(savedTransaction);
+            assertThat(savedTransaction).isNotNull();
+            
+        }catch(ResourceNotFoundException e){
+            System.out.print(e);
+        }
+
+        // then - verify the output
+    }
+
+
 
     
 }
